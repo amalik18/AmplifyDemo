542bb1dfd48151d174c5d886415454607e937da2
amalik18 <ali_malik96@yahoo.com>
2020-12-03 01:55:38 -0500

    Attempt fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import React from 'react';
+// import { withAuthenticator, AmplifySignOut, AmplifySignIn } from '@aws-amplify/ui-react';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello from v2!!!
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//       <AmplifySignOut/>
+//     </div>
+//   );
+// }
+
+// export default withAuthenticator(App);
+
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import { withAuthenticator, AmplifySignOut, AmplifySignIn } from '@aws-amplify/ui-react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello from v2!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>We now have Auth!</h1>
       </header>
-      <AmplifySignOut/>
+      <AmplifySignOut />
     </div>
   );
 }
